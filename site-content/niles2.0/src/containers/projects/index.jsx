fb4e3031e9b39fc5df06080d1015449a306d690c
riju-talk <talk.riju@gmail.com>
2024-03-18 11:20:58 +0530

    started with projects page

--- site-content/niles2.0/src/containers/projects/index.jsx
+++ site-content/niles2.0/src/containers/projects/index.jsx
@@ -4,8 +4,22 @@ function Project(){
     return (
         <div>
             <h1>Projects,</h1>
+            <div className="section__this_site">
+            <img src="" alt="main page screenshot"/>
             <p id="decription">Projects;</p>
-            <img src="" alt="" style={{ width: '200px', height: '200px' }} />
+            </div>
+            <div className="section__note_making_app">
+            <img src="" alt="note making and list making app"/>
+            <p id="decription">Projects;</p>
+            </div>
+            <div className="project for Statistical Machine Learning course">
+            <img src="" alt="s"/>
+            <p id="decription">Projects;</p>
+            </div>
+            <div className="section__ecommerce_website">
+            <img src="" alt="project for Database Managment Systems course"/>
+            <p id="decription">Projects;</p>
+            </div>
         </div>
     )
 }
