63071289a6032298001729a90668504ba750a040
riju-talk <talk.riju@gmail.com>
2024-03-24 21:27:48 +0530

    site 65% complete final tweaks needed

--- site-content/niles2.0/src/containers/card/index.jsx
+++ site-content/niles2.0/src/containers/card/index.jsx
@@ -8,7 +8,8 @@ function Card({ image, title, body }) {
             <img src={image} class="card-img-top img-prop" alt="image-content" />
             <div class="card-body">
                 <h3 class="card-title title-color">{title}</h3>
-                <p class="card-text content-text-color">{body}</p>
+                <p class="card-text content-text-color">{body[0]}</p>
+                <p class="card-text content-text-color">{body[1]}</p>
             </div>
         </div>
     );
