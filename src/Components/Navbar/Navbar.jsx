b42b23dfb91c6dedac5bf1dd7362ed2faa4e731a
Phantom_Cloak18 <131883829+rijusmit22400@users.noreply.github.com>
2024-06-21 17:05:38 +0530

    navbar init

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 import './Navbar.css';
 
 function Navbar() {
-    return <div className="w-80 p-3 bar">
+    return <div className="w-80 p-3 bar container">
         <nav class="border rounded-5">
-            <ul className="nav justify-content-center nav-underline">
+            <ul className="nav justify-content-center container">
                 <li className="nav-item"><Link to="/home" className="nav-link">Home</Link></li>
                 <li className="nav-item"><Link to="/projects" className="nav-link">Projects</Link></li>
                 <li className="nav-item"><Link to="/social" className="nav-link">Social</Link></li>
