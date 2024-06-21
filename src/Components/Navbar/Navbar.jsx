0de5c9d2392398bdb271771c7ea43b2dfc61bf5a
Phantom_Cloak18 <131883829+rijusmit22400@users.noreply.github.com>
2024-06-21 18:19:18 +0530

    minor fixings done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,11 +4,11 @@ import './Navbar.css';
 
 function Navbar() {
     return <div className="w-80 p-3 bar container">
-        <nav class="border rounded-5">
+        <nav className="navbar navbar-expand-lg border rounded-5 nav-justified">
             <ul className="nav justify-content-center container">
-                <li className="nav-item"><Link to="/home" className="nav-link">Home</Link></li>
-                <li className="nav-item"><Link to="/projects" className="nav-link">Projects</Link></li>
-                <li className="nav-item"><Link to="/social" className="nav-link">Social</Link></li>
+                <li className="flex-sm-fill text-sm-center nav-item"><Link to="/home" className="nav-link">Home</Link></li>
+                <li className="flex-sm-fill text-sm-center nav-item"><Link to="/projects" className="nav-link">Projects</Link></li>
+                <li className="flex-sm-fill text-sm-center nav-item"><Link to="/social" className="nav-link">Social</Link></li>
             </ul>
         </nav>
     </div>
