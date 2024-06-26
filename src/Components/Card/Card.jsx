ddb62a3ca914b7a6bcc4e7f5d8b772117bb9f2ae
Phantom_Cloak18 <131883829+rijusmit22400@users.noreply.github.com>
2024-06-26 20:06:54 +0530

    responsive site, active links and social box init

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import "./Card.css";
+import { Row, Col, Image } from "react-bootstrap";
 
-function Card({id, im, title, discription}) {
+function Card({ id, im, title, description }) {
     return (
         <div className="card" id={id}>
-            <div className="card-body">
-                <div className="card-title">
-                    {title}
-                </div>
-                <div className="card-text">
-                    {discription}
-                </div>
-            </div>
+            <Row>
+                <Col xs={4} md={4} lg={4} xl={4} sm={4} className="d-flex align-items-center">
+                    <Image className="card-img-top modified border" src={im} alt="Card image cap" />
+                </Col>
+                <Col>
+                    <div className="card-body">
+                        <div className="card-title">{title}</div>
+                        <div className="card-text">{description}</div>
+                    </div>
+                </Col>
+            </Row>
         </div>
     );
 }
